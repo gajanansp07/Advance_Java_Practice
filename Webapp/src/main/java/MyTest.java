5111b1ba65d2cd1db2ae96b24710bec268979177
gajanansp07 <gajanansp07@gmail.com>
2022-04-02 20:32:44 +0530

    added Spring Boot

--- Webapp/src/main/java/MyTest.java
+++ Webapp/src/main/java/MyTest.java
@@ -1,5 +1,4 @@
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,15 +10,17 @@ import java.sql.DriverManager;
 
 public class MyTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			pw.println("Driver loaded.....");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhot:1521:xe","admin","admin");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "admin", "admin");
 			pw.println("Connection Eshtabilished..........");
 		} catch (Exception e) {
-			pw.println("Welcom"+e.getMessage());
+			pw.println("Welcom" + e.getMessage());
 			e.printStackTrace();
 		}
 	}
